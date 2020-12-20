9f3be5589371ec8b6a6b01851a8fab4b739d7f8b
Raul Ratiu <raulratiu2@gmail.com>
2020-12-20 09:19:17 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@
 setTimeout(function () {
   $(".preloader").fadeToggle();
 }, 1500);
+
+/* Text Effect */
+
+$(function(){
+  $('.repeat').click(function(){
+      var classes =  $(this).parent().attr('class');
+        $(this).parent().attr('class', 'animate');
+        var indicator = $(this);
+        setTimeout(function(){ 
+          $(indicator).parent().addClass(classes);
+        }, 20);
+    });
+});
